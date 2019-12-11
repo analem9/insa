f7bbbcdf7cca815cec80c88565758669f8ffe1fe
analem9 <anais.nalem@laposte.net>
2019-12-11 08:18:56 +0100

    qunitjs tests suite et fin

---
+++ tp2/dist/built.js
@@ -0,0 +1,10 @@
+function concatenateStr(string) {
+    var concatened = "Bonjour ".concat(string);
+    return concatened;
+};function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
+}
+
--- tp2/test/tests.js
+++ tp2/test/tests.js
@@ -8,12 +8,12 @@ QUnit.test( "hello test", function( assert ) {
 
 // Test /js/isEven.js 
 QUnit.test( "isEven tests", function(assert) {
-    assert.expect(5);
+    assert.expect(4);
     var iseven0 = isEven(0);
     var iseven1 = isEven(1);
     var iseven2 = isEven(2);
     var isevenm1 = isEven(-1);
-    var isevenm2 = isEven(-2);
+    // var isevenm2 = isEven(-2);
     // assert.ok(iseven0, true, "O is even.");
     // assert.notOk(iseven1, false, "1 is odd.");
     // assert.ok(iseven2, true, "2 is even.");
@@ -23,7 +23,7 @@ QUnit.test( "isEven tests", function(assert) {
     assert.notOk(iseven1,"1 is odd.");
     assert.ok(iseven2, "2 is even.");
     assert.notOk(isevenm1, "-1 is odd.");
-    assert.ok(isevenm2, "-2 is even.");
+    // assert.ok(isevenm2, "-2 is even.");
 });
 
 // Test /js/concatenateStr.js 
